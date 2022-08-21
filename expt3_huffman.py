624b640f59a9ffb4d94ac8da694f4ff14314ae6f
voluxgit <108188310+voluxgit@users.noreply.github.com>
2022-08-21 21:27:22 +0530

    Update expt3_huffman.py

--- expt3_huffman.py
+++ expt3_huffman.py
@@ -85,8 +85,7 @@ print('Made by Varad Patil 120A2036')
 
 
 
-Enter the input in this way :
-![image](https://user-images.githubusercontent.com/108188310/185799667-f21f2212-c139-4077-bf0d-583e961b05b6.png)
+
 
 
 
